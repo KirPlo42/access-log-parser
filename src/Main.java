8dfbd23ec394cfa625f23ff495ddb8241e1d8c36
Kirill Plodukhin <ploduhin@dev.vtb.ru>
2023-05-01 20:48:33 +0300

    Change project for experiments with numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.printf("Длина текста: %d", text.length());
+        System.out.println("Введите первое число (a):");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число (b):");
+        int b = new Scanner(System.in).nextInt();
+        System.out.printf("a + b = %d \n", a + b);
+        System.out.printf("a - b = %d \n", a - b);
+        System.out.printf("a * b = %d \n", a * b);
+        System.out.printf("a / b = %s", (double) a / b);
     }
 }
