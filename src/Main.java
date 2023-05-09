5e42ca322cd56f85a4b4b0b5ff46ef2ee41c1a6b
Kirill Plodukhin <ploduhin@dev.vtb.ru>
2023-05-09 17:53:12 +0300

    Add code for adding files with check

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.printf("Длина текста: %d", text.length());
+        int counter = 0;
+        while(true) {
+            System.out.println("Please enter path to the file");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory){
+                System.out.printf("This is path (%s) to the directory \n", path);
+                continue;
+            }
+            if (!fileExists){
+                System.out.printf("File %s is not exist \n", path);
+                continue;
+            }
+            System.out.println("This is correct path to the file");
+            counter++;
+            System.out.printf("Number for this file - %d \n", counter);
+        }
+
     }
 }
