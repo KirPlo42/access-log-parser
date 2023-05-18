162018d0b04194008d8707102d5beb5f50636234
Kirill Plodukhin <ploduhin@dev.vtb.ru>
2023-05-18 19:11:34 +0300

    - Add exercises with broken lines

---
+++ src/BrokenLine.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrokenLine {
+    List<CoordinateDot> dots;
+
+    public BrokenLine(List<CoordinateDot> dots) {
+        this.dots = dots;
+    }
+
+    static BrokenLine ofEmpty() {
+        List<CoordinateDot> dots = new ArrayList<>();
+        return new BrokenLine(dots);
+    }
+
+    static BrokenLine ofDots(List<CoordinateDot> dotsList) {
+        return new BrokenLine(dotsList);
+    }
+
+    @Override
+    public String toString() {
+        if (dots.size() == 0) {
+            return "We have empty broken line";
+        }
+        String result = "";
+        for (CoordinateDot dot : dots) {
+            result += dot.toString() + ",";
+        }
+        return "Line:" + result.substring(0, result.length() - 1);
+    }
+
+    public List<Line> whatLinesDoWeHaveList() {
+        List<Line> result = new ArrayList<>();
+        for (int i = 1; i < dots.size(); i++) {
+            Line line = Line.ofDots(dots.get(i - 1), dots.get(i));
+            result.add(line);
+        }
+        return result;
+    }
+
+    public double lengthByLines(){
+        double result = 0;
+        for (Line line: whatLinesDoWeHaveList()) {
+             result += line.length();
+        }
+        return result;
+    }
+
+    public double lengthByFormula(){
+        double result = 0;
+        for (int i = 1; i < dots.size(); i++) {
+            result += Math.sqrt(Math.pow((dots.get(i).x - dots.get(i-1).x), 2) + Math.pow((dots.get(i).y - dots.get(i-1).y), 2));
+        }
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -23,5 +26,35 @@ public class Main {
         //line from coordinates
         Line line4 = Line.ofCoordinates(1,2,3,4);
         System.out.println(line4);
+
+
+        //BrokenLines part
+        System.out.println("\n\n\nPart of broken lines");
+        System.out.println("Task 1");
+        List<CoordinateDot> dots = Arrays.asList(new CoordinateDot(1,5), new CoordinateDot(2,8), new CoordinateDot(5,3), new CoordinateDot(8,9));
+        BrokenLine brokenLine1 = BrokenLine.ofDots(dots);
+        System.out.println(brokenLine1);
+
+        System.out.println("Task 2");
+        System.out.println(brokenLine1.lengthByFormula());
+
+        System.out.println("Task 3");
+        System.out.println(brokenLine1.whatLinesDoWeHaveList());
+
+        System.out.println("Task 4");
+        System.out.println(brokenLine1.whatLinesDoWeHaveList().size());
+
+        System.out.println("Task 5");
+        System.out.println(brokenLine1.lengthByLines() == brokenLine1.lengthByFormula());
+
+        System.out.println("task 6");
+        dots.get(1).x = 12;
+        dots.get(1).y = 8;
+        System.out.println(brokenLine1);
+        System.out.println(brokenLine1.whatLinesDoWeHaveList());
+
+        System.out.println("Line without dotes");
+        BrokenLine brokenLine2 = BrokenLine.ofEmpty();
+        System.out.println(brokenLine2);
     }
 }
