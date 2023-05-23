1accd5c7fd68b6318e0fac49af0eda3187f60e90
Kirill Plodukhin <ploduhin@dev.vtb.ru>
2023-05-23 22:10:33 +0300

    - Add exercises with fractions

---
+++ src/Fraction.java
@@ -0,0 +1,54 @@
+public class Fraction {
+    final int numerator;
+    final int divider;
+
+    public Fraction(int numerator, int divider) throws Exception {
+        if (divider <= 0)
+            throw new IllegalArgumentException("divider must be positive");
+        this.numerator = numerator;
+        this.divider = divider;
+    }
+
+    public static Fraction sumOrMinus(Fraction fraction1, Fraction fraction2, Boolean invertSecondNumerator) throws Exception {
+        int tempSecondNum = fraction2.numerator;
+        if (invertSecondNumerator)
+            tempSecondNum = tempSecondNum * (-1);
+        int tempDivider = minMultiple(fraction1.divider, fraction2.divider);
+        int tempNum = fraction1.numerator * (tempDivider / fraction1.divider) + tempSecondNum * (tempDivider / fraction2.divider);
+        return new Fraction(tempNum, tempDivider);
+    }
+
+    public Fraction sum(Fraction fraction) throws Exception {
+        return sumOrMinus(this, fraction, false);
+    }
+
+    public Fraction sum(int number) throws Exception {
+        return sumOrMinus(this, new Fraction(number, 1), false);
+    }
+
+    public Fraction minus(Fraction fraction) throws Exception {
+        return sumOrMinus(this, fraction, true);
+    }
+
+    public Fraction minus(int number) throws Exception {
+        return sumOrMinus(this, new Fraction(number, 1), true);
+    }
+
+
+    private static int bothDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return bothDivisor(b, a % b);
+    }
+
+    private static int minMultiple(int a, int b) {
+        return a * b / bothDivisor(a, b);
+    }
+
+    @Override
+    public String toString() {
+        return numerator +
+                "/" + divider;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,9 @@
-import java.io.File;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        int counter = 0;
-        while(true) {
-            System.out.println("Please enter path to the file");
-            String path = new Scanner(System.in).nextLine();
-            File file = new File(path);
-            boolean fileExists = file.exists();
-            boolean isDirectory = file.isDirectory();
-            if (isDirectory){
-                System.out.printf("This is path (%s) to the directory \n", path);
-                continue;
-            }
-            if (!fileExists){
-                System.out.printf("File %s is not exist \n", path);
-                continue;
-            }
-            System.out.println("This is correct path to the file");
-            counter++;
-            System.out.printf("Number for this file - %d \n", counter);
-        }
+    public static void main(String[] args) throws Exception {
 
+        Fraction f1 = new Fraction(1,3);
+        Fraction f2 = new Fraction(2,5);
+        Fraction f3 = new Fraction(7,8);
+        System.out.println(f1.sum(f2).sum(f3).minus(5));
     }
 }
